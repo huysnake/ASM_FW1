78e47711b70aae08a007a48e10e47fe539b8318d
huysnake <105479758+huysnake@users.noreply.github.com>
2023-05-28 11:08:17 +0700

    CRUD [ LIST SP ]

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
+import { ProductEditComponent } from './pages/product-edit/product-edit.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductDetailComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    AdminLayoutComponent,
+    ProductListComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
